9823284a36f8e4198dc15d9e5d11e3c6aefb651e
techlearning8 <techlearning8@gmail.com>
2022-05-30 06:39:27 +0530

    changes

---
+++ javascript/Practice/Functions/EasyLevel1.js
@@ -0,0 +1,10 @@
+// 1. Write a JavaScript function that reverse a number. 
+// Example x = 32243;
+// Expected Output : 34223
+
+
+function reversNum(x){
+ console.log(x.split(""));
+}
+reversNum(231);
+
